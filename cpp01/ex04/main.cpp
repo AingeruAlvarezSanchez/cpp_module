e025f37dd14a48c62bce32004a0e0a54e4fa1be5
AingeruAlvarezSanchez <aingeru.alvarez@outlook.com>
2022-10-19 21:04:50 +0200

    all cpp01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,8 +3,7 @@
 
 void	replaceWordsInFile(std::ifstream &inputFile, std::string fileName, std::string stringToErase, std::string stringToInsert)
 {
-	std::ofstream	outputFile;
-	outputFile.open(fileName.append(".replace"), std::ofstream::out | std::ofstream::trunc);
+	std::ofstream	outputFile(fileName.append(".replace"));
 	std::string 	bufferToRead;
 	u_int			posToInsertNewString;
 
@@ -25,13 +24,11 @@ int	main(int argc, char **argv)
 {
 	if (argc != 4)
 		return (std::cout << "ERROR" << std::endl,1);
-	std::ifstream	inputFile;
-	inputFile.open(argv[1], std::ifstream::in);
-	if (!inputFile.is_open() || inputFile.peek() == std::string::npos)
+	std::ifstream	inputFile(argv[1]);
+	if (!inputFile.is_open() || inputFile.peek() == EOF)
 		return (inputFile.close(), std::cout << "ERROR, FILE IS EMPTY OR DOES NOT EXIST" << std::endl, 1);
 	if (!argv[2][0] || !argv[3][0])
 		return (std::cout << "ERROR, ONE OR MORE STRINGS ARE EMPTY" << std::endl, 1);
 	replaceWordsInFile(inputFile, argv[1], argv[2], argv[3]);
-	inputFile.close();
 	return (0);
 }
